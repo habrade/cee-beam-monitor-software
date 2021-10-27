11c51e1a353ca10ca362209a4275a1fcb36733c9
sdong <s.dong@mails.ccnu.edu.cn>
2021-10-27 23:11:44 +0800

    initial files for data analysis

---
+++ data_analysis/data_analysis.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+data_dir = "./data/"
+
+file_name = "arst1.1_30k_200mV_in.dat"
+
+file_path = data_dir + file_name
+
+print(file_path)
+
+c = np.fromfile(file_path, dtype=int32)
+
+print(type(c))
